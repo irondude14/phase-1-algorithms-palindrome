09962a158a11e14a3a3645354c8d4b9615eaf003
Vladyslav <vkadyrov@protonmail.com>
2022-10-13 15:30:55 -0400

    commit tree 44346de7c4376fd672c4f24007af9bf4fd3e4c58

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let check = /[^A-Za-z0-9]/g;
+  let lowReg = word.toLowerCase().replace(check, '');
+  let reverStr = lowReg.split('').reverse().join('');
+  return reverStr === lowReg
 }
 
 /* 
-  Add your pseudocode here
+  1. To start we need to lowercase all the words that we passing in to the function; - done w/ .toLowerCase()
+  2. We need to remove/replace all special/unwanted characters from the palindrome check; - done w/ .replace() & RegExp /[^A-Za-z0-9]/g
+  3. We need to break strings into letters, reverse them & assable again; - done w/ chain of .split() .reverse() .join(), for .split() & .join() we shoud pass empty string ''
+  4. We need to check that first & last, second & next to last and so on, letters were matching; - we need to compare lower register string to reverse string;
+  5. We need to return true;
 */
 
 /*
